57866768c13a9d1904fe629cc533a3db8ddda10f
Sarthak Johnson Prasad <sarthakjohnsonprasad@gmail.com>
2020-04-12 14:03:06 +0530

    Move scrapers, new regex

--- extractor/youtube/scrapers.py
+++ extractor/youtube/scrapers.py
@@ -8,7 +8,7 @@ def scrape_yt(soup):
     raw_title = soup.find('meta', {'property': 'og:title'}).get('content').strip()
     artist,title = None,None
 
-    #In case the YouTube Title is in good format,i.e, Artist-Song("foo")
+    #In case the YouTube Title is in the commonly used format <Artist> - <Song name>
     if '-' in raw_title:
         raw_title =  get_artist_title(raw_title)
         artist, title = raw_title.split(' - ')
@@ -16,14 +16,14 @@ def scrape_yt(soup):
     # In case the  YouTube Title only contains song name    
     else :
         title = get_artist_title(raw_title)
-        artist = soup.find('a',class_= "yt-uix-sessionlink spf-link").text
+        artist = soup.find('a',class_= "yt-uix-sessionlink spf-link").text  #Scrapes "Artist" from the YouTube Channel name 
         artist = clean_channel(artist)
 
     info = {'Category': None , 'Song' :title, 'Artist':artist, 'Album':None, 'Licensed to YouTube by' : None}
     return info
 
 # Scrapper function for videos with "Music in this video" section
-def scrape_yt_new(soup):
+def scrape_embedded_yt_metadata(soup):
     tags = soup.find_all('li', class_= "watch-meta-item yt-uix-expander-body")
 
     info = {'Category': None, 'Song':None, 'Artist': None, 'Album':None, 'Licensed to YouTube by':None}
@@ -43,6 +43,6 @@ def scrape(url):
     soup = BeautifulSoup(response.content, 'lxml')
 
     if(len(soup.find_all('li',class_="watch-meta-item yt-uix-expander-body")) > 1 ):
-        return scrape_yt_new(soup)
+        return scrape_embedded_yt_metadata(soup)
     return scrape_yt(soup)
 
--- extractor/youtube/validator.py
+++ extractor/youtube/validator.py
@@ -1,7 +1,7 @@
 import re
 
 def yt_validate(yt_link) :
-        if(bool(re.match("^https://\w{0,3}.?youtube+\.\w{2,3}/watch\?v=[\w-]{11}$", yt_link)) or bool(re.match("^https://youtu.be/", yt_link))):
+        if(bool(re.match("http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?",yt_link))):
                 return True
         return False
 
@@ -9,3 +9,5 @@ def is_short_link(yt_link):
         if(bool(re.match("^https://youtu.be/", yt_link))):
                 return True
         return False
+
+
