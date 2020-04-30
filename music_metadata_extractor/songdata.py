73bef4f734f289ef5e02675d63ff8eb4500ebaa2
Sarthak Johnson Prasad <sarthakjohnsonprasad@gmail.com>
2020-04-30 20:47:21 +0530

    Add logging warning

--- music_metadata_extractor/songdata.py
+++ music_metadata_extractor/songdata.py
@@ -2,7 +2,7 @@ from typing import List
 from .resolver import get_provider_input
 from .spotify import SpotifyProvider
 from .models import Track, Artist, BaseProviderInput, BaseProvider
-
+import logging
 
 class SongData:
     """Contains all metadata to be consumed by users of this package"""
@@ -14,7 +14,8 @@ class SongData:
             provider_data: BaseProvider = SpotifyProvider(provider_input).data
             self.track: Track = provider_data.track
             self.artists: List[Artist] = provider_data.artists
-        except IndexError:
+        except IndexError as e:
+            logging.warning(e, stack_info=True)
             self.track: Track = Track(provider_id=None, name=provider_input.song_name, is_cover=None, original_id=None, popularity=None, year=None, explicit=None, image_id=None, genre=[])
             self.artists: List[Artist] = []
 
