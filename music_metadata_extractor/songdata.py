cdbf46067be9cbbd736bc61b9a614ac44ce8a5a5
ghostwriternr <ghostwriternr@gmail.com>
2020-05-27 07:49:13 +0530

    Fix typo

--- music_metadata_extractor/songdata.py
+++ music_metadata_extractor/songdata.py
@@ -17,18 +17,18 @@ class SongData:
             self.artists: List[Artist] = provider_data.artists
             self.extraAttrs: dict = extraAttrs
         except ValueError as ve:
-            logging.warning(ve, stack_info=True)
+            logging.warning(ve)
             raise ve
         except IndexError as ie:
             if str(ie) == "No data found in Spotify":
-                logging.warning(ie, stack_info=True)
+                logging.warning(ie)
                 raise ValueError("No data found on Spotify") from ie
         except AttributeError as ae:
             if str(ae) == "Error while parsing YouTube page":
-                logging.warning(ae, stack_info=True)
-                raise ValueError("Error while parsing YouTube page") from ie
+                logging.warning(ae)
+                raise ValueError("Error while parsing YouTube page") from ae
         except Exception as e:
-            logging.error(e, stack_info=True)
+            logging.error(e)
             raise ValueError("Unable to fetch metadata for input link") from e
 
     def __repr__(self):
