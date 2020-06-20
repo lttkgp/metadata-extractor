b8f8cfe55bdce34ac29fc0d60c423b6297a8af7d
Naresh R <ghostwriternr@gmail.com>
2020-06-20 16:04:02 +0530

    Bump version to 1.0.0 :sparkles:

--- setup.py
+++ setup.py
@@ -16,12 +16,12 @@ install_requires = [
     "youtube-title-parse==1.0.0",
 ]
 
-test_requires = ["pytest==5.4.1"]
+tests_require = ["pytest==5.4.1"]
 
 # This call to setup() does all the work
 setup(
     name="music_metadata_extractor",
-    version="1.0.0b2",
+    version="1.0.0",
     description="Fetch music metadata from common Music APIs for a variety of data sources",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -37,6 +37,6 @@ setup(
     packages=find_packages(exclude=["contrib", "docs", "tests*"]),
     include_package_data=True,
     install_requires=install_requires,
-    test_requires=test_requires,
+    tests_require=tests_require,
     entry_points={"console_scripts": []},
 )
